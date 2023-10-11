f81a0c7ff37a71cbea5013624804968bf3c6ae89
punit <punitvishwakarma47@gmail.com>
2023-10-11 15:04:15 +0530

    final project

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 300
+}); 
+
+// Navbar 
+
+
+//Heaing strat
+ScrollReveal().reveal('.info', { origin:'left' });
+ScrollReveal().reveal('.my-img', { origin:'right'});
+
+
+ScrollReveal().reveal('.img-tow', { origin:'left'})
+
+ScrollReveal().reveal('.about-text', { origin:'right'})
+
+// section 2 skills start
+
+ScrollReveal().reveal('.skills-left', { origin:'left'})
+ScrollReveal().reveal('.skills-right', { orogin:'right'})
+
+// section 3 project starts 
+
+ScrollReveal().reveal('.main-1', { origin:'left'})
+ScrollReveal().reveal('.main-2', { origin:'right'})
+
+// section 4 contact strats 
+
+ScrollReveal().reveal('.form', { origin:'left'})
+ScrollReveal().reveal('.map', { origin:'right'})
