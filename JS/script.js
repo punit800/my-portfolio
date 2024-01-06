6476a8660769cbfcb92600599e067f5e07d1ed2e
Delta Student <punitvishwakarma47@gmail.com>
2024-01-06 16:11:53 +0530

    final project

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,3 @@
-//  var typed = new Typed(".text", {
-//     Strings:["Frontend Developer", "Web Developer"],
-//     typeSpeed: 100,
-//     backSpeed: 100,
-//     backDelay: 1000,
-//     loop: true
-// });
 
 var typed = new Typed('#element', {
     strings: ['Frontend Developer.', 'Web Developer'],
