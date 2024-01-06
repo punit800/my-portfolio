fce428c209cc0a2edf4d682718576f795dfd3653
Delta Student <punitvishwakarma47@gmail.com>
2024-01-06 15:49:40 +0530

    final project

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,10 @@
+let typed = new typed(".text", {
+    Strings:["Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
 
 ScrollReveal({
     reset: true,
